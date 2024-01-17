bc2c91c0eb2ed967add1d7a6de36212f2d245fb4
NGR\groa <gonzalo.roa@ngr.com.pe>
2024-01-17 12:05:05 -0500

    fix: resolucion conflictos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -85,15 +85,3 @@ export const environment = {
   //clientsecret: 'mgps0alo77vpdviokkumq2njk3vggki7rnam9ho02sluvliigen',
 
 };
-
-
-
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
