863d298deb8906814487f60e9930fbe5a2164233
Luis Acosta <luis.acosta@ngr.com.pe>
2024-04-02 16:57:29 -0500

    cambios agregadores

---
+++ src/app/interfaz/entity/logInterfaz.ts
@@ -0,0 +1,10 @@
+export class InterfazLog {
+    public idInterfazLog!: number;
+    public dfecinicio!: string;
+    public dfecfin!: string;
+    public cdusuario!: string;
+    public vatiendas!: string;
+    public mensaje!: string;
+    public dfecregistro!: Date;
+    public vatipointerfaz: string;
+}
