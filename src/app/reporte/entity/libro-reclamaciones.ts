59b8c3cae5167bd5b1ec46a2cb34579fa79e1c1a
acutti <alexander.cutti@ngr.com.pe>
2024-04-03 23:14:19 -0500

    Ligro de Reclamaciones

---
+++ src/app/reporte/entity/libro-reclamaciones.ts
@@ -0,0 +1,22 @@
+export class LibroReclamaciones {
+    id: string;
+    idmarca: number;
+    marca!: string;
+    codigo!: string;
+    nombre: string;
+    departamento: string;
+    direccion: string;
+    dni: string;
+    telefono: string;
+    email: string;
+    datospadres: string;
+    biendelcontrato: string;
+    montoreclamado: string;
+    motivoreclamado: string;
+    tienda: string;
+    tipo: string;
+    tipodealle: string;
+    nombrearchivo: string;
+    observacionesyacciones: string;
+    fecha: Date;
+}
